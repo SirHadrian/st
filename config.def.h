04be782c209b3098279a42711c6128425aaaaa8c
Toma Adrian <sirhadrian21@gmail.com>
2023-10-13 12:25:57 +0300

    alpha and font update

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font =
-    "CaskaydiaCove Nerd Font Mono:pixelsize=18:antialias=true:autohint=true";
+    "CaskaydiaCove Nerd Font Mono:pixelsize=20:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -95,7 +95,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.5;
+float alpha = 0.3;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
